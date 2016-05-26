de031baaa0235b82392b21c43a9500759f41b286
Andrew Carter <acarter@flipboard.com>
2016-05-26 14:14:13 -0700

    Add Bootstrap::Git::Config module
    With get and set methods

--- lib/git.rb
+++ lib/git.rb
@@ -4,14 +4,21 @@
 
 module Bootstrap
   module Git
-    def config(key, value)
-      system "git config --global #{key} \"#{value}\""
+    module Config
+      def get(key)
+        return %x{git config --global --get #{key}}.strip
+      end
+      module_function :get
+      
+      def set(key, value)
+        system %Q{git config --global #{key} "#{value}"}
+      end
+      module_function :set
     end
-    module_function :config
   
     def clone(repo, dest=nil)
       git_url = URI.join("https://github.com/", "#{repo}.git").to_s
-      system "git clone #{git_url} #{dest ? dest.to_s : ''}"
+      system %Q{git clone #{git_url} #{dest ? dest.to_s : ''}}
     end
     module_function :clone
 
