1833657fa9dde125c20f875659aef39d76fdf1ca
Andrew Carter <ascarter@uw.edu>
2017-02-15 23:42:15 -0800

    Add Yarn helpers

--- lib/node.rb
+++ lib/node.rb
@@ -57,4 +57,55 @@ module Bootstrap
     end
     module_function :ls
   end
+
+  # Yarn helpers
+  module Yarn
+    def install(pkg)
+      if !installed?(pkg)
+        Bootstrap.sudo "yarn global add #{pkg}"
+      else
+        warn "#{pkg} already installed"
+      end
+    end
+    module_function :install
+
+    def update(pkg = '')
+      Bootstrap.sudo "yarn global update #{pkg}"
+    end
+    module_function :update
+
+    def uninstall(pkg)
+      if installed?(pkg)
+        Bootstrap.sudo "yarn global remove #{pkg}"
+      else
+        warn "#{pkg} is not installed"
+      end
+    end
+    module_function :uninstall
+
+    def installed?(pkg)
+    	list.each do |p|
+    		return true if pkg == p.split('@')[0]
+    	end
+    	return false
+    end
+    module_function :installed?
+
+    def list
+      packages = []
+      `sudo yarn global ls --no-emoji --no-progress 2>&1`.split("\n").each do |line|
+      	line.match(/^info "(\S+)"/) do |m|
+      		packages.push(m[1])
+      	end
+      end
+      packages
+    end
+    module_function :list
+
+    def ls
+      puts 'Installed global yarn modules:'
+      list.each { |pkg| puts " #{pkg}" }
+    end
+    module_function :ls
+  end
 end
