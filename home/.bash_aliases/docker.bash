3d685e496f0cf2d66cad6db164ebaf437190a8a3
Andrew Carter <asc@snoqualmiecoding.com>
2017-08-16 11:07:10 -0500

    Remove unused alias

--- home/.bash_aliases/docker.bash
+++ home/.bash_aliases/docker.bash
@@ -5,8 +5,5 @@
 # Remove stopped containers
 alias dockerrms='docker rm $(docker ps -a -q)'
 
-# Remove all untagged images
-#alias dockerrmi='docker rmi $(docker images -q --filter "dangling=true")'
-
 # List untagged images
 alias dockeruntagged='docker images --filter "dangling=true"'
