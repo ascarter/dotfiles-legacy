208cda93737daefd03affff358f7d460656409ba
Andrew Carter <acarter@flipboard.com>
2017-10-04 09:33:15 -0700

    Remove unneccesary ssh alias

--- home/.bash_aliases/macos.bash
+++ home/.bash_aliases/macos.bash
@@ -5,10 +5,6 @@ Darwin )
 	# ls
 	alias ls='ls -hFGH'
 
-	# SSH init
-	# Initialize ssh agent and add keys
-	alias sshi='eval "$(ssh-agent -s)" && ssh-add -A'
-
 	# System shortcuts
 	alias lockscreen='/System/Library/CoreServices/"Menu Extras"/User.menu/Contents/Resources/CGSession -suspend'
 
