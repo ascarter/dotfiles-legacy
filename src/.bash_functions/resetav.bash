28febd3c89192de9eebe102813675fbcd39fab43
Andrew Carter <acarter@flipboard.com>
2019-04-03 11:13:27 -0700

    Add macOS reset av helpers

---
+++ src/.bash_functions/resetav.bash
@@ -0,0 +1,15 @@
+#  -*- mode: unix-shell-script; -*-
+
+resetaudio() {
+	sudo killall coreaudiod
+}
+
+resetvideo() {
+	sudo killall VDCAssistant
+	sudo killall AppleCameraAssistant
+}
+
+resetav() {
+	resetaudio()
+	resetvideo()
+}
