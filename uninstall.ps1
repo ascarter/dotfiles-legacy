1af5dc8dac3c8f6e28cb43e314c46358d6adcbce
Andrew Carter <acarter@microsoft.com>
2020-01-02 08:54:50 -0800

    Add windows uninstall script

---
+++ uninstall.ps1
@@ -0,0 +1,22 @@
+<#
+.SYNOPSIS
+    Install script for Windows 10
+.DESCRIPTION
+    Configure dotfiles configuration for current Windows user
+.PARAMETER Verbose
+	Display diagnostic information
+#>
+[cmdletbinding()]
+param()
+
+Set-StrictMode -Version Latest
+$ErrorActionPreference = "Stop"
+$ProgressPreference = "SilentlyContinue"
+
+# Require administrator
+if (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+    Write-Error "Insufficient privileges"
+}        
+
+# Remove user profile
+if (!(Test-Path -Path $PROFILE.CurrentUserAllHosts)) { Remove-Item -Path $PROFILE.CurrentUserAllHosts }
