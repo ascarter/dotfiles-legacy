89bbf5e6b610be4c0f9682c989fbb7ff65e0304f
Andrew Carter <acarter@microsoft.com>
2020-01-11 23:26:23 -0800

    Windows software install update
    * Remove modules (let those get installed by profile as needed)
    * Fix install
    * Relaunch using Windows PowerShell if needed

--- wininstall.ps1
+++ wininstall.ps1
@@ -11,29 +11,17 @@ Set-StrictMode -Version Latest
 $ErrorActionPreference = "Stop"
 
 # TODO: Check that running in powershell *not* powershell core
+if ($PSVersionTable.PSEdition -ne "Desktop") {
+    $relaunchArgs = "& '" + $MyInvocation.MyCommand.Definition + "'"
+    Start-Process powershell -ArgumentList $relaunchArgs -NoNewWindow
+    Break
+}
 
 # Use TLS 1.2
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
 # Dotfiles enlistment
 $dotfiles = Join-Path -Path $env:USERPROFILE -ChildPath .config\dotfiles
-
-function Install-PSModules() {
-    # PowerShellGet
-    if (!(Get-Module -Name PowerShellGet -All)) {
-        Install-Module -Name PowerShellGet -Scope CurrentUser -Force -AllowClobber
-    }
-
-
-    if (!(Get-Module -Name Microsoft.PowerShell.GraphicalTools -All)) {
-        Install-Module Microsoft.PowerShell.GraphicalTools
-    }
-
-    # posh-git
-    if (!(Get-Module -Name posh-git -All)) {
-        Install-Module -Name posh-git -Scope CurrentUser -AllowPrerelease -Force
-    }
-}
 
 function Install-Packages() {
     # Read package catalog
@@ -47,7 +35,7 @@ function Install-Packages() {
     foreach ($p in $packages) {
         $idx++
         $useSudo = if (Get-Member -Name sudo -InputObject $p) { $p.sudo } else { $false }
-        $args = if (Get-Member -Name args -InputObject $p) { $p.args } else { $null }
+        $args = if (Get-Member -Name args -InputObject $p) { $p.args } else { " " }
         $completed = [math]::Round((($idx - 1) / $packages.Count) * 100)
 
         if ($installed | Where-Object { $_.CanonicalID -eq $p.id }) {
@@ -58,15 +46,15 @@ function Install-Packages() {
 
         try {
             # Download file
-            Write-Progress -Activity $activity -CurrentOperation "Downloading $($p.url)" -Id 1 -Status "$completed% complete" -PercentComplete $completed
+            Write-Progress -Activity $activity -CurrentOperation "Downloading $($p.url)" -Id 1 -Status "$($p.id)" -PercentComplete $completed
             $target = Join-Path -Path $env:TEMP -ChildPath (Split-Path -Path $p.url -Leaf)
             $wc = New-Object System.Net.WebClient
-            $wc.DownloadFile($gitUri, $target)
+            $wc.DownloadFile($p.url, $target)
 
             # Execute installer
-            Write-Progress -Activity $activity -CurrentOperation "Installing $(Split-Path -Path $target -Leaf)" -Id 1 -Status "$completed% complete" -PercentComplete $completed
+            Write-Progress -Activity $activity -CurrentOperation "Installing $(Split-Path -Path $target -Leaf)" -Id 1 -Status "$($p.id)" -PercentComplete $completed
             $verb = if ($useSudo) { 'RunAs' } else { 'Open' }
-            $exe = switch ($target.Extension) {
+            $exe = switch ((Get-Item -Path $target).Extension) {
                 .exe { $target }
                 .msi { $args += ('/I', $target); 'msiexec.exe' }
                 Default { throw "Unknown file type for $file" }
@@ -78,10 +66,9 @@ function Install-Packages() {
             Write-Warning "[$($_.Exception.GetType().FullName)] $($_.Exception.Message)"
         }
         finally {
-            if (Test-Path -Path $target) { Remove-Item -Path $file -Force }
+            if (Test-Path -Path $target) { Remove-Item -Path $target -Force }
         }
     }
 }
 
-Install-PSModules
 Install-Packages
