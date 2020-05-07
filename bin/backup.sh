1bcc087df4b242a7b71848e401ea826ed701fdc1
Andrew Carter <acarter@microsoft.com>
2020-05-06 17:39:37 -0700

    Remove unused scripts

--- bin/backup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# Usage:
-# backup.sh [source_dir] [dest]
-# source == /Users/andrew
-# dest   == /Volumes/MyDrive/backup (will copy to /Volumes/MyDrive/backup/andrew)
-
-rsync -av --delete-excluded --exclude=${1}/Dropbox --exclude=${1}/.dropbox --exclude=${1}/Library/Caches "${1}" "${2}"
--- bin/outputcolors.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-# Output colors: http://linuxtidbits.wordpress.com/2008/08/11/output-color-on-bash-scripts/
-for i in $(seq 0 $(tput colors)); do
-	echo " $(tput setaf $i)Text$(tput sgr0) $(tput bold)$(tput setaf $i)Text$(tput sgr0) $(tput sgr 0 1)$(tput setaf $i)Text$(tput sgr0)  \$(tput setaf $i)"
-done
