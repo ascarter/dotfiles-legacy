e44e421c80a66513277fb3c889245690e8614abf
Andrew Carter <acarter@microsoft.com>
2020-05-06 17:01:17 -0700

    Update powershell config

--- install.ps1
+++ install.ps1
@@ -6,9 +6,9 @@
 #>
 [cmdletbinding()]
 param(
-	[Parameter(HelpMessage = "Skip system configuration")]
-	[switch]
-	$NoSystem = $false
+    [Parameter(HelpMessage = "Skip system configuration")]
+    [switch]
+    $NoSystem = $false
 )
 
 Set-StrictMode -Version Latest
@@ -16,9 +16,9 @@ $ErrorActionPreference = "Stop"
 
 # Run as administrator
 If (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
-	$relaunchArgs = "& '" + $MyInvocation.MyCommand.Definition + "'"
-	Start-Process powershell -Verb RunAs -ArgumentList $relaunchArgs
-	Break
+    $relaunchArgs = "& '" + $MyInvocation.MyCommand.Definition + "'"
+    Start-Process powershell -Verb RunAs -ArgumentList $relaunchArgs
+    Break
 }
 
 #region Setup
@@ -46,62 +46,62 @@ $gitCmd = Join-Path -Path $env:ProgramFiles -ChildPath "Git\cmd\git.exe"
 #region Bootstrap
 
 function Install-Git() {
-	if (!(Test-Path -Path $gitCmd)) {
-		try {
-			$gitInstaller = Split-Path $gitURI -Leaf
-			$target = Join-Path -Path $env:TEMP -ChildPath $gitInstaller
-			Write-Host "Installing Git $gitInstaller"
-			$wc = New-Object System.Net.WebClient
-			$wc.DownloadFile($gitUri, $target)
-			Start-Process -FilePath $target -Wait -NoNewWindow
-		}
-		finally {
-			if (Test-Path $target) { Remove-Item -Path $target }
-		}
-	}
+    if (!(Test-Path -Path $gitCmd)) {
+        try {
+            $gitInstaller = Split-Path $gitURI -Leaf
+            $target = Join-Path -Path $env:TEMP -ChildPath $gitInstaller
+            Write-Host "Installing Git $gitInstaller"
+            $wc = New-Object System.Net.WebClient
+            $wc.DownloadFile($gitUri, $target)
+            Start-Process -FilePath $target -Wait -NoNewWindow
+        }
+        finally {
+            if (Test-Path $target) { Remove-Item -Path $target }
+        }
+    }
 
-	# Set Git SSH client
-	if ($null -eq [System.Environment]::GetEnvironmentVariable("GIT_SSH", "User")) {
-		[System.Environment]::SetEnvironmentVariable("GIT_SSH", (Get-Command ssh.exe).Path, [System.EnvironmentVariableTarget]::User)
-	}
+    # Set Git SSH client
+    if ($null -eq [System.Environment]::GetEnvironmentVariable("GIT_SSH", "User")) {
+        [System.Environment]::SetEnvironmentVariable("GIT_SSH", (Get-Command ssh.exe).Path, [System.EnvironmentVariableTarget]::User)
+    }
 }
 
 function Install-SSHKeys() {
-	# Create SSH key if not present
-	if (!(Test-Path $sshKeyFile)) {
-		Write-Host "Generating SSH user key"
-		$githubEmail = Read-Host -Prompt "Enter GitHub email address"
-		ssh-keygen -t ed25519 -C "$githubEmail"
-		ssh-add $sshKeyFile
+    # Create SSH key if not present
+    if (!(Test-Path $sshKeyFile)) {
+        Write-Host "Generating SSH user key"
+        $githubEmail = Read-Host -Prompt "Enter GitHub email address"
+        ssh-keygen -t ed25519 -C "$githubEmail"
+        ssh-add $sshKeyFile
 
-		# Copy SSH public key to clipboard
-		$sshPublicKeyFile = $sshKeyFile + '.pub'
-		if ($PSVersionTable.PSVersion.Major -lt 6) {
-			Get-Content -Path $sshPublicKeyFile | Set-Clipboard
-		}
-		else {
-			Invoke-WinCommand -ScriptBlock { Get-Content -Path $sshPublicKeyFile | Set-Clipboard }
-		}
-		Write-Host "ssh public key copied to clipboard."
-		Write-Host "Add key to GitHub account"
-		Start-Process "https://github.com/settings/ssh/new"
-		Write-Host "After adding to GitHub, Press any key to continue..."
-		[void][System.Console]::ReadKey($true)
-	}
+        # Copy SSH public key to clipboard
+        $sshPublicKeyFile = $sshKeyFile + '.pub'
+        if ($PSVersionTable.PSVersion.Major -lt 6) {
+            Get-Content -Path $sshPublicKeyFile | Set-Clipboard
+        }
+        else {
+            Invoke-WinCommand -ScriptBlock { Get-Content -Path $sshPublicKeyFile | Set-Clipboard }
+        }
+        Write-Host "ssh public key copied to clipboard."
+        Write-Host "Add key to GitHub account"
+        Start-Process "https://github.com/settings/ssh/new"
+        Write-Host "After adding to GitHub, Press any key to continue..."
+        [void][System.Console]::ReadKey($true)
+    }
 }
 
 function Install-Dotfiles() {
-	# Clone dotfiles
-	if (!(Test-Path -Path $dotfiles)) {
-		Write-Host "Clone dotfiles"
-		$repo = if (Test-Path -Path $sshKeyfile) { "git@github.com:$dotfilesRepo" } else { "https://github.com/$dotfilesRepo" }
-		Start-Process -FilePath $gitCmd -ArgumentList "clone $repo $dotfiles" -Wait -NoNewWindow
-	}
+    # Clone dotfiles
+    if (!(Test-Path -Path $dotfiles)) {
+        Write-Host "Clone dotfiles"
+        $repo = if (Test-Path -Path $sshKeyfile) { "git@github.com:$dotfilesRepo" } else { "https://github.com/$dotfilesRepo" }
+        Start-Process -FilePath $gitCmd -ArgumentList "clone $repo $dotfiles" -Wait -NoNewWindow
+    }
 }
 
 function Install-Profiles() {
-	$setprofiles = Join-Path -Path $dotfiles -ChildPath setprofiles.ps1
-	if (Test-Path -Path $setprofiles) { Start-Process -FilePath powershell -ArgumentList $setprofiles -Wait -NoNewWindow }
+    $setprofiles = Join-Path -Path $dotfiles -ChildPath setprofiles.ps1
+    if (Test-Path -Path $setprofiles) { Start-Process -FilePath powershell -ArgumentList $setprofiles -Wait -NoNewWindow }
 }
 
 #endregion
@@ -109,33 +109,33 @@ function Install-Profiles() {
 #region System Configuration
 
 function Install-SSH() {
-	# Install OpenSSH
-	# https://docs.microsoft.com/en-us/windows-server/administration/openssh/openssh_install_firstuse
-	Add-WindowsCapability -Online -Name OpenSSH.Client
-	Add-WindowsCapability -Online -Name OpenSSH.Server
+    # Install OpenSSH
+    # https://docs.microsoft.com/en-us/windows-server/administration/openssh/openssh_install_firstuse
+    Add-WindowsCapability -Online -Name OpenSSH.Client
+    Add-WindowsCapability -Online -Name OpenSSH.Server
 
-	# Add firewall rule
-	if (!((Get-NetFirewallRule -Name "OpenSSH-Server-In-TCP").Enabled -eq $true)) {
-		Write-Warning "Missing OpenSSH Server inbound firewall rule"
-	}
+    # Add firewall rule
+    if (!((Get-NetFirewallRule -Name "OpenSSH-Server-In-TCP").Enabled -eq $true)) {
+        Write-Warning "Missing OpenSSH Server inbound firewall rule"
+    }
 
-	# Install OpenSSHUtils
-	Install-Module -Name OpenSSHUtils -Scope AllUsers -Force
+    # Install OpenSSHUtils
+    Install-Module -Name OpenSSHUtils -Scope AllUsers -Force
 
-	Set-Service -Name ssh-agent -StartupType 'Automatic'
-	Start-Service ssh-agent
-	Set-Service -Name sshd -StartupType 'Automatic'
-	Start-Service sshd
+    Set-Service -Name ssh-agent -StartupType 'Automatic'
+    Start-Service ssh-agent
+    Set-Service -Name sshd -StartupType 'Automatic'
+    Start-Service sshd
 
-	# Configure default shell
-	New-ItemProperty -Path HKLM:\SOFTWARE\OpenSSH -Name DefaultShell -Value $env:ProgramFiles\PowerShell\6\pwsh.exe
+    # Configure default shell
+    New-ItemProperty -Path HKLM:\SOFTWARE\OpenSSH -Name DefaultShell -Value $env:ProgramFiles\PowerShell\6\pwsh.exe
 }
 
 function Install-Virtualization() {
-	Enable-WindowsOptionalFeature -Online -FeatureName Containers-DisposableClientVM -All
-	Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V -All
-	Enable-WindowsOptionalFeature -Online -FeatureName VirtualMachinePlatform -All
-	Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux -All
+    Enable-WindowsOptionalFeature -Online -FeatureName Containers-DisposableClientVM -All
+    Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V -All
+    Enable-WindowsOptionalFeature -Online -FeatureName VirtualMachinePlatform -All
+    Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux -All
 }
 
 #endregion
@@ -149,8 +149,8 @@ Install-Profiles
 
 # Run system configuration
 if (-not $NoSystem) {
-	Install-SSH
-	Install-Virtualization
+    Install-SSH
+    Install-Virtualization
 }
 
 Write-Output "Dotfiles install complete"
--- profile.ps1
+++ profile.ps1
@@ -1,15 +1,15 @@
 # Bootstrap modules
 
 if (!(Get-Module -Name PowerShellGet -ListAvailable)) {
-  Install-Module -Name PowerShellGet -Scope CurrentUser -Force -AllowClobber
+    Install-Module -Name PowerShellGet -Scope CurrentUser -Force -AllowClobber
 }
 
 if (!(Get-Module -Name Microsoft.PowerShell.GraphicalTools -ListAvailable)) {
-  Install-Module Microsoft.PowerShell.GraphicalTools -Scope CurrentUser -Force -AllowClobber
+    Install-Module Microsoft.PowerShell.GraphicalTools -Scope CurrentUser -Force -AllowClobber
 }
 
 if (!(Get-Module -Name posh-git -ListAvailable)) {
-  Install-Module -Name posh-git -Scope CurrentUser -AllowPrerelease -Force
+    Install-Module -Name posh-git -Scope CurrentUser -AllowPrerelease -Force
 }
 
 # Define top level bin (like /usr/local/bin in Unix)
@@ -17,6 +17,10 @@ $Env:LOCALBIN = Join-Path -Path $Env:SystemDrive -ChildPath bin
 
 # Extend PATH to include LOCALBIN
 $Env:Path = "$Env:LOCALBIN;$Env:Path"
+
+if (Test-Path -Path (Join-Path $Env:LOCALAPPDATA "Fork")) {
+    $Env:Path = "$Env:Path;" + (Join-Path $Env:LOCALAPPDATA "Fork")
+}
 
 # Set DOTFILES environment varible
 Set-Item -Path Env:DOTFILES -Value (Join-Path $env:USERPROFILE -ChildPath ".config\dotfiles")
@@ -32,30 +36,23 @@ Set-Item -Path Env:VISUAL -Value ((Get-Command code).Source)
 # Alias behavoirs
 
 function Start-Fork([string]$MyRepo = $PWD) {
-  $exe = $Env:LOCALAPPDATA + '\Fork\Fork.exe'
-  $target = Convert-Path -Path $MyRepo
-  & $exe $target
-}
-
-function Start-Insomnia() {
-  $exe = $Env:HOMEPATH + '\bin\insomnia.exe'
-  Start-Process -FilePath $exe -WindowStyle Minimized
+    $target = Convert-Path -Path $MyRepo
+    & Fork.exe $target
 }
 
 function Start-1Password() {
-  if ($null -eq $env:OP_SESSION_carters) {
-    Invoke-Expression $(op signin carters)
-  }
+    if ($null -eq $env:OP_SESSION_carters) {
+        Invoke-Expression $(op signin carters)
+    }
 }
 
 function Set-LocationDotfiles() {
-  Set-Location -Path $env:DOTFILES
+    Set-Location -Path $env:DOTFILES
 }
 
 Set-Alias -Name dev -Value Start-DevEnv
-Set-Alias -Name dotf -Value Set-LocationDotfiles
+Set-Alias -Name dotfiles -Value Set-LocationDotfiles
 Set-Alias -Name fork -Value Start-Fork
-Set-Alias -Name insomnia -Value Start-Insomnia
 Set-Alias -Name opsignin -Value Start-1Password
 Set-Alias -Name opssh -Value Get-SSHPassphrase
 
@@ -74,75 +71,75 @@ Set-Alias -Name pbpaste -Value Get-Clipboard
 
 # Get-Password retrieves a password for a 1Password password item
 function Get-SSHPassphrase([string]$Key = $env:COMPUTERNAME.ToLower()) {
-  $sshKey = 'ssh ' + $Key
-  Start-1Password
-  op get item $sshKey | jq -r '.details.password' | Set-Clipboard
+    $sshKey = 'ssh ' + $Key
+    Start-1Password
+    op get item $sshKey | jq -r '.details.password' | Set-Clipboard
 }
 
 function Start-DevEnv() {
-  & { Import-Module (Join-Path ${env:ProgramFiles(x86)} "Microsoft Visual Studio\2019\Enterprise\Common7\Tools\Microsoft.VisualStudio.DevShell.dll"); Enter-VsDevShell da341a44 }
+    & { Import-Module (Join-Path ${env:ProgramFiles(x86)} "Microsoft Visual Studio\2019\Enterprise\Common7\Tools\Microsoft.VisualStudio.DevShell.dll"); Enter-VsDevShell da341a44 }
 }
 
 function Start-ProfileEdit { code -n $PROFILE.CurrentUserAllHosts }
 
 function Update-Profiles() {
-  $setprofiles = Join-Path -Path $env:DOTFILES -ChildPath setprofiles.ps1
-  if (Test-Path -Path $setprofiles) {
-    Start-Process -FilePath (Get-Process -Id $pid).ProcessName -ArgumentList "$setprofiles -Force"  -Wait -NoNewWindow
-  }
+    $setprofiles = Join-Path -Path $env:DOTFILES -ChildPath setprofiles.ps1
+    if (Test-Path -Path $setprofiles) {
+        Start-Process -FilePath (Get-Process -Id $pid).ProcessName -ArgumentList "$setprofiles -Force"  -Wait -NoNewWindow
+    }
 }
 
 function Update-VSCodeExtensions() {
-  $extensions = Get-Content -Path (Join-Path -Path $env:DOTFILES -ChildPath '.\vscode-extensions.txt')
-  foreach ($extension in $extensions) { code --install-extension $extension }
+    $extensions = Get-Content -Path (Join-Path -Path $env:DOTFILES -ChildPath '.\vscode-extensions.txt')
+    foreach ($extension in $extensions) { code --install-extension $extension }
 }
 
 function Get-Uname() {
-  Get-CimInstance Win32_OperatingSystem | Select-Object 'Caption', 'CSName', 'Version', 'BuildType', 'OSArchitecture' | Format-Table
+    Get-CimInstance Win32_OperatingSystem | Select-Object 'Caption', 'CSName', 'Version', 'BuildType', 'OSArchitecture' | Format-Table
 }
 
 function Get-IsAdmin() {
-  ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
+    ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
 }
 
 # gitconfig
 
 function gc_set([string]$Key, [string]$Value) {
-  git config --global $Key $Value | Out-Null
+    git config --global $Key $Value | Out-Null
 }
 
 function gc_update([string]$Key, [string]$Value) {
-  git config --global --unset $Key
-  gc_set $Key $Value
+    git config --global --unset $Key
+    gc_set $Key $Value
 }
 
 function gc_prompt([string]$Key, [string]$Prompt) {
-  $default = git config --global --get $Key
-  $msg = if ($null -eq $default) { $Prompt } else { "$Prompt (default $default)" }
-  $value = Read-Host -Prompt $msg
-  if ($null -eq $value) { $value = $default }
-  gc_set $Key $value
+    $default = git config --global --get $Key
+    $msg = if ($null -eq $default) { $Prompt } else { "$Prompt (default $default)" }
+    $value = Read-Host -Prompt $msg
+    if ($null -eq $value) { $value = $default }
+    gc_set $Key $value
 }
 
 function Update-GitConfig() {
-  # Include defaults and aliases
-  gc_update 'include.path' (Join-Path -Path $env:DOTFILES -ChildPath gitconfig)
+    # Include defaults and aliases
+    gc_update 'include.path' (Join-Path -Path $env:DOTFILES -ChildPath gitconfig)
 
-  # User info
-  gc_prompt 'user.name' "User name"
-  gc_prompt 'user.email' "Email"
+    # User info
+    gc_prompt 'user.name' "User name"
+    gc_prompt 'user.email' "Email"
 
-  # Platform configuration
-  gc_set 'gui.fontui' '-family \"Segoe UI\" -size 10 -weight normal -slant roman -underline 0 -overstrike 0'
-  gc_set 'gui.fontdiff' '-family \"Cascadia Code\" -size 10 -weight normal -slant roman -underline 0 -overstrike 0'
+    # Platform configuration
+    gc_set 'gui.fontui' '-family \"Segoe UI\" -size 10 -weight normal -slant roman -underline 0 -overstrike 0'
+    gc_set 'gui.fontdiff' '-family \"Cascadia Code\" -size 10 -weight normal -slant roman -underline 0 -overstrike 0'
 
-  # Show full gitconfig
-  Write-Output $(git config --global --list)
+    # Show full gitconfig
+    Write-Output $(git config --global --list)
 }
 
 
 function Get-CmdletAlias ($cmdletname) {
-  Get-Alias | Where-Object -FilterScript { $_.Definition -like "$cmdletname" } | Format-Table -Property Definition, Name -AutoSize
+    Get-Alias | Where-Object -FilterScript { $_.Definition -like "$cmdletname" } | Format-Table -Property Definition, Name -AutoSize
 }
 
 # Default prompt with ADMIN and DBG
@@ -159,25 +156,25 @@ function Get-CmdletAlias ($cmdletname) {
 
 # Configure posh-git
 if (Get-Module -Name posh-git -ListAvailable) {
-  Import-Module posh-git
+    Import-Module posh-git
 
-  function prompt {
-    $identity = [Security.Principal.WindowsIdentity]::GetCurrent()
-    $principal = [Security.Principal.WindowsPrincipal] $identity
+    function prompt {
+        $identity = [Security.Principal.WindowsIdentity]::GetCurrent()
+        $principal = [Security.Principal.WindowsPrincipal] $identity
 
-    $GitPromptSettings.DefaultPromptPrefix.Text = "`n[$env:COMPUTERNAME] "
-    $GitPromptSettings.DefaultPromptBeforeSuffix.Text = '`n' + $(
-      if (Test-Path variable:/PSDebugContext) { Write-Prompt '[DBG]: ' -ForegroundColor Red }
-      elseif ($principal.IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Write-Prompt '[ADMIN]: ' -ForegroundColor Magenta }
-    )
-    $GitPromptSettings.DefaultPromptSuffix.Text = "PS > "
-    $prompt = & $GitPromptScriptBlock
-    if ($prompt) { $prompt } else { ' ' }
-  }
+        $GitPromptSettings.DefaultPromptPrefix.Text = "`n[$env:COMPUTERNAME] "
+        $GitPromptSettings.DefaultPromptBeforeSuffix.Text = '`n' + $(
+            if (Test-Path variable:/PSDebugContext) { Write-Prompt '[DBG]: ' -ForegroundColor Red }
+            elseif ($principal.IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Write-Prompt '[ADMIN]: ' -ForegroundColor Magenta }
+        )
+        $GitPromptSettings.DefaultPromptSuffix.Text = "PS > "
+        $prompt = & $GitPromptScriptBlock
+        if ($prompt) { $prompt } else { ' ' }
+    }
 }
 
 function Get-InstalledSoftware {
-  <#
+    <#
 .SYNOPSIS
   Retrieves a list of all software installed on a Windows computer.
 .EXAMPLE
@@ -193,110 +190,110 @@ function Get-InstalledSoftware {
 .PARAMETER Guid
   The software GUID you'e like to limit the query to
 #>
-  [CmdletBinding()]
-  param (
+    [CmdletBinding()]
+    param (
 
-    [Parameter()]
-    [ValidateNotNullOrEmpty()]
-    [string]$ComputerName = $env:COMPUTERNAME,
+        [Parameter()]
+        [ValidateNotNullOrEmpty()]
+        [string]$ComputerName = $env:COMPUTERNAME,
 
-    [Parameter()]
-    [ValidateNotNullOrEmpty()]
-    [string]$Name,
+        [Parameter()]
+        [ValidateNotNullOrEmpty()]
+        [string]$Name,
 
-    [Parameter()]
-    [guid]$Guid,
+        [Parameter()]
+        [guid]$Guid,
 
-    [Parameter(Mandatory = $false)]
-    [switch]$Gui
-  )
-  process {
-    try {
-      $scriptBlock = {
-        $args[0].GetEnumerator() | ForEach-Object { New-Variable -Name $_.Key -Value $_.Value }
+        [Parameter(Mandatory = $false)]
+        [switch]$Gui
+    )
+    process {
+        try {
+            $scriptBlock = {
+                $args[0].GetEnumerator() | ForEach-Object { New-Variable -Name $_.Key -Value $_.Value }
 
-        $UninstallKeys = @(
-          "HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall",
-          "HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall"
-        )
+                $UninstallKeys = @(
+                    "HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall",
+                    "HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall"
+                )
 
-        if (Get-IsAdmin) {
-          New-PSDrive -Name HKU -PSProvider Registry -Root Registry::HKEY_USERS | Out-Null
-          $UninstallKeys += Get-ChildItem HKU: | Where-Object { $_.Name -match 'S-\d-\d+-(\d+-){1,14}\d+$' } | ForEach-Object {
-            "HKU:\$($_.PSChildName)\Software\Microsoft\Windows\CurrentVersion\Uninstall"
-          }
-        }
+                if (Get-IsAdmin) {
+                    New-PSDrive -Name HKU -PSProvider Registry -Root Registry::HKEY_USERS | Out-Null
+                    $UninstallKeys += Get-ChildItem HKU: | Where-Object { $_.Name -match 'S-\d-\d+-(\d+-){1,14}\d+$' } | ForEach-Object {
+                        "HKU:\$($_.PSChildName)\Software\Microsoft\Windows\CurrentVersion\Uninstall"
+                    }
+                }
 
-        if (-not $UninstallKeys) {
-          Write-Warning -Message 'No software registry keys found'
-        }
-        else {
-          foreach ($UninstallKey in $UninstallKeys) {
-            $friendlyNames = @{
-              'DisplayName'    = 'Name'
-              'DisplayVersion' = 'Version'
+                if (-not $UninstallKeys) {
+                    Write-Warning -Message 'No software registry keys found'
+                }
+                else {
+                    foreach ($UninstallKey in $UninstallKeys) {
+                        $friendlyNames = @{
+                            'DisplayName'    = 'Name'
+                            'DisplayVersion' = 'Version'
+                        }
+                        Write-Verbose -Message "Checking uninstall key [$($UninstallKey)]"
+                        if ($Name) {
+                            $WhereBlock = { $_.GetValue('DisplayName') -like "$Name*" }
+                        }
+                        elseif ($GUID) {
+                            $WhereBlock = { $_.PsChildName -eq $Guid.Guid }
+                        }
+                        else {
+                            $WhereBlock = { $_.GetValue('DisplayName') }
+                        }
+                        $SwKeys = Get-ChildItem -Path $UninstallKey -ErrorAction SilentlyContinue | Where-Object $WhereBlock
+                        if (-not $SwKeys) {
+                            Write-Verbose -Message "No software keys in uninstall key $UninstallKey"
+                        }
+                        else {
+                            foreach ($SwKey in $SwKeys) {
+                                $output = @{ }
+                                $output.Name = $SwKey
+                                foreach ($ValName in $SwKey.GetValueNames()) {
+                                    if ($ValName -ne 'Version') {
+                                        $output.InstallLocation = ''
+                                        if ($ValName -eq 'InstallLocation' -and
+                                            ($SwKey.GetValue($ValName)) -and
+                                            (@('C:', 'C:\Windows', 'C:\Windows\System32', 'C:\Windows\SysWOW64') -notcontains $SwKey.GetValue($ValName).TrimEnd('\'))) {
+                                            $output.InstallLocation = $SwKey.GetValue($ValName).TrimEnd('\')
+                                        }
+                                        [string]$ValData = $SwKey.GetValue($ValName)
+                                        if ($friendlyNames[$ValName]) {
+                                            $output[$friendlyNames[$ValName]] = $ValData.Trim() ## Some registry values have trailing spaces.
+                                        }
+                                        else {
+                                            $output[$ValName] = $ValData.Trim() ## Some registry values trailing spaces
+                                        }
+                                    }
+                                }
+                                $output.GUID = ''
+                                if ($SwKey.PSChildName -match '\b[A-F0-9]{8}(?:-[A-F0-9]{4}){3}-[A-F0-9]{12}\b') {
+                                    $output.GUID = $SwKey.PSChildName
+                                }
+                                New-Object -TypeName PSObject -Prop $output
+                            }
+                        }
+                    }
+                }
             }
-            Write-Verbose -Message "Checking uninstall key [$($UninstallKey)]"
-            if ($Name) {
-              $WhereBlock = { $_.GetValue('DisplayName') -like "$Name*" }
-            }
-            elseif ($GUID) {
-              $WhereBlock = { $_.PsChildName -eq $Guid.Guid }
+
+            if ($ComputerName -eq $env:COMPUTERNAME) {
+                $results = Invoke-Command -ScriptBlock $scriptBlock -ArgumentList $PSBoundParameters
             }
             else {
-              $WhereBlock = { $_.GetValue('DisplayName') }
+                $results = Invoke-Command -ComputerName $ComputerName -ScriptBlock $scriptBlock -ArgumentList $PSBoundParameters
             }
-            $SwKeys = Get-ChildItem -Path $UninstallKey -ErrorAction SilentlyContinue | Where-Object $WhereBlock
-            if (-not $SwKeys) {
-              Write-Verbose -Message "No software keys in uninstall key $UninstallKey"
+            if ($Gui) {
+                $results | Out-GridView -Title "Installed Software"
             }
             else {
-              foreach ($SwKey in $SwKeys) {
-                $output = @{ }
-                $output.Name = $SwKey
-                foreach ($ValName in $SwKey.GetValueNames()) {
-                  if ($ValName -ne 'Version') {
-                    $output.InstallLocation = ''
-                    if ($ValName -eq 'InstallLocation' -and
-                      ($SwKey.GetValue($ValName)) -and
-                      (@('C:', 'C:\Windows', 'C:\Windows\System32', 'C:\Windows\SysWOW64') -notcontains $SwKey.GetValue($ValName).TrimEnd('\'))) {
-                      $output.InstallLocation = $SwKey.GetValue($ValName).TrimEnd('\')
-                    }
-                    [string]$ValData = $SwKey.GetValue($ValName)
-                    if ($friendlyNames[$ValName]) {
-                      $output[$friendlyNames[$ValName]] = $ValData.Trim() ## Some registry values have trailing spaces.
-                    }
-                    else {
-                      $output[$ValName] = $ValData.Trim() ## Some registry values trailing spaces
-                    }
-                  }
-                }
-                $output.GUID = ''
-                if ($SwKey.PSChildName -match '\b[A-F0-9]{8}(?:-[A-F0-9]{4}){3}-[A-F0-9]{12}\b') {
-                  $output.GUID = $SwKey.PSChildName
-                }
-                New-Object -TypeName PSObject -Prop $output
-              }
+                $results | Format-Table -AutoSize -Property Name, Publisher, Version, GUID
             }
-          }
         }
-      }
-
-      if ($ComputerName -eq $env:COMPUTERNAME) {
-        $results = Invoke-Command -ScriptBlock $scriptBlock -ArgumentList $PSBoundParameters
-      }
-      else {
-        $results = Invoke-Command -ComputerName $ComputerName -ScriptBlock $scriptBlock -ArgumentList $PSBoundParameters
-      }
-      if ($Gui) {
-        $results | Out-GridView -Title "Installed Software"
-      }
-      else {
-        $results | Format-Table -AutoSize -Property Name, Publisher, Version, GUID
-      }
+        catch {
+            Write-Error -Message "Error: $($_.Exception.Message) - Line Number: $($_.InvocationInfo.ScriptLineNumber)"
+        }
     }
-    catch {
-      Write-Error -Message "Error: $($_.Exception.Message) - Line Number: $($_.InvocationInfo.ScriptLineNumber)"
-    }
-  }
 }
