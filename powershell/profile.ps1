5b95c6962c383627225469807374eff54f655e8c
Andrew Carter <asc@alpental.io>
2020-09-17 14:20:28 -0700

    Add winget tab completion

--- powershell/profile.ps1
+++ powershell/profile.ps1
@@ -86,6 +86,20 @@ Update-Path @(
     (Join-Path -Path $Env:LOCALAPPDATA -ChildPath "Fork")
 )
 
+# Enable winget completion
+if (Get-Command winget -ErrorAction SilentlyContinue) {
+    Register-ArgumentCompleter -Native -CommandName winget -ScriptBlock {
+        param($wordToComplete, $commandAst, $cursorPosition)
+
+        [Console]::InputEncoding = [Console]::OutputEncoding = $OutputEncoding = [System.Text.Utf8Encoding]::new()
+        $Local:word = $wordToComplete.Replace('"', '""')
+        $Local:ast = $commandAst.ToString().Replace('"', '""')
+        winget complete --word="$Local:word" --commandline "$Local:ast" --position $cursorPosition | ForEach-Object {
+            [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
+        }
+    }
+}
+
 #endregion
 
 #region Alias behavoirs
