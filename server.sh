740d8689d9db9e16b84cc1161e06a9fcd6d98ab9
Andrew Carter <asc@alpental.io>
2021-01-22 23:11:45 -0800

    Add docker-compose to server install

--- server.sh
+++ server.sh
@@ -49,6 +49,11 @@ Linux )
 
         # Add user to docker group
         sudo usermod -aG docker $USER
+
+        # Install docker-compose
+        sudo curl -L "https://github.com/docker/compose/releases/download/1.28.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+        sudo chmod +x /usr/local/bin/docker-compose
+        sudo curl -L https://raw.githubusercontent.com/docker/compose/1.28.0/contrib/completion/bash/docker-compose -o /etc/bash_completion.d/docker-compose
 		;;
 	*)
 		echo "Unknown Linux distro ${DISTRO_DESCRIPTION}"
