5eff6083c8bc514b9e99cb9dbd4d19ea0804bbf7
Andrew Carter <asc@alpental.io>
2021-01-24 18:04:57 -0800

    Notes for docker-compose server install

--- server.sh
+++ server.sh
@@ -51,8 +51,9 @@ Linux )
         sudo usermod -aG docker $USER
 
         # Install docker-compose
-        sudo curl -L "https://github.com/docker/compose/releases/download/1.28.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-        sudo chmod +x /usr/local/bin/docker-compose
+        # sudo curl -L "https://github.com/docker/compose/releases/download/1.28.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+        # sudo chmod +x /usr/local/bin/docker-compose
+        # pip3 install docker-compose
         sudo curl -L https://raw.githubusercontent.com/docker/compose/1.28.0/contrib/completion/bash/docker-compose -o /etc/bash_completion.d/docker-compose
 		;;
 	*)
