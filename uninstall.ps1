971701d54d3ed53805c41f8fc7b845d680fc8160
Andrew Carter <asc@alpental.io>
2021-09-14 10:16:50 -0700

    Remove dotfiles on uninstall

--- uninstall.ps1
+++ uninstall.ps1
@@ -12,14 +12,17 @@ $ErrorActionPreference = "Stop"
 $ProgressPreference = "SilentlyContinue"
 
 # Remove user profile
-if (Test-Path -Path $PROFILE) { Remove-Item -Path $PROFILE }
+if (Test-Path -Path $PROFILE) { Remove-Item -Path $PROFILE -Force }
 
 # Remove vimrc
 $vimrc = Join-Path -Path $env:USERPROFILE -ChildPath _vimrc
-if (Test-Path -Path $vimrc) { Remove-Item -Path $vimrc }
+if (Test-Path -Path $vimrc) { Remove-Item -Path $vimrc -Force }
 
 # Remove gitconfig
 $gitconfig = Join-Path -Path $env:USERPROFILE -ChildPath .gitconfig
-if (Test-Path -Path $gitconfig) { Remove-Item -Path $gitconfig }
+if (Test-Path -Path $gitconfig) { Remove-Item -Path $gitconfig -Force }
+
+# Remove dotfiles
+if (Test-Path $Env:DOTFILES) { Remove-Item -Path $Env:DOTFILES -Force}
 
 Write-Output "dotfiles uninstalled"
