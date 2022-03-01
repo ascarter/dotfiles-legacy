575ff349ecc7d1b00e311fb626906bdd1dc5afe8
Andrew Carter <asc@alpental.io>
2022-03-01 00:29:01 -0800

    Update pop install

--- developer.sh
+++ developer.sh
@@ -13,8 +13,6 @@ Linux )
 
     case $(lsb_release -i -s) in
     Ubuntu | Pop )
-        ARCH=$(dpkg --print-architecture)
-
         # Ensure time is in sync (drift can occur on WSL or VM)
         sudo hwclock -s
 
@@ -36,10 +34,11 @@ Linux )
                                 g++ \
                                 gcc \
                                 git \
-                                gnome-tweak-tool \
+                                gnome-tweaks \
                                 gnupg \
                                 gnupg-agent \
                                 gparted \
+                                htop \
                                 jq \
                                 make \
                                 mc \
@@ -53,19 +52,23 @@ Linux )
                                 vim-gtk3
 
         # Add Pop packages
-        if [ $(lsb_release -i -s) == "Pop" ]; then
+        if [ "$(lsb_release -i -s)" = "Pop" ]; then
             sudo apt-get install -y gnome-remote-desktop gnome-user-share
         fi
 
         # Add GitHub CLI repository
         # https://github.com/cli/cli/blob/trunk/docs/install_linux.md#debian-ubuntu-linux-apt
-        sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-key C99B11DEB97541F0
-        sudo apt-add-repository https://cli.github.com/packages
+        curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
+        echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+        sudo apt-get update
+        sudo apt-get install -y gh
 
         # Add Microsoft repository
         # https://docs.microsoft.com/en-us/windows-server/administration/linux-package-repository-for-microsoft-software#ubuntu
         curl -sSL https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
-        sudo apt-add-repository https://packages.microsoft.com/ubuntu/20.04/prod
+        sudo apt-add-repository https://packages.microsoft.com/ubuntu/$(lsb_release -r -s)/prod
+        sudo apt-get update
+        sudo apt-get install -y dotnet-sdk-6.0 msopenjdk-17 powershell
 
         # Add nodesource repository
         # https://github.com/nodesource/distributions/blob/master/README.md#debinstall
@@ -74,7 +77,10 @@ Linux )
         # Add Yarn
         curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | gpg --dearmor | sudo tee /usr/share/keyrings/yarnkey.gpg >/dev/null
         echo "deb [signed-by=/usr/share/keyrings/yarnkey.gpg] https://dl.yarnpkg.com/debian stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-
+        sudo apt-get update
+        sudo apt-get install -y nodejs yarn
+        sudo npm install --global typescript
+        
         # Add 1Password repository
         if ! [ -f /usr/share/keyrings/1password-archive-keyring.gpg ]; then
             curl -sS https://downloads.1password.com/linux/keys/1password.asc | sudo gpg --dearmor --output /usr/share/keyrings/1password-archive-keyring.gpg
@@ -88,26 +94,17 @@ Linux )
             sudo mkdir -p /usr/share/debsig/keyrings/AC2D62742012EA22
             curl -sS https://downloads.1password.com/linux/keys/1password.asc | sudo gpg --dearmor --output /usr/share/debsig/keyrings/AC2D62742012EA22/debsig.gpg
         fi
+        sudo apt-get update
+        sudo apt-get install -y 1password
 
         # Add speedtest respository
         # https://www.speedtest.net/apps/cli
         curl -s https://install.speedtest.net/app/cli/install.deb.sh | sudo bash
-
-        # Install packages
         sudo apt-get update
-        sudo apt-get install -y 1password \
-                                dotnet-sdk-6.0 \
-                                gh \
-                                msopenjdk-17 \
-                                nodejs \
-                                powershell \
-                                speedtest \
-                                yarn \
-        sudo npm install --global typescript
+        sudo apt-get install -y speedtest
 
         # Install latest Go in /usr/local
-        # curl "https://go.dev/dl/?mode=json" | jq --arg filter "go.*.$(uname -s)-$(uname -m)" -r '.[0].files[].filename | select(test($filter; "i"))'
-        GO_VERSION=$(curl -s "https://go.dev/dl/?mode=json" | jq --arg os $(uname -s | tr '[:upper:]' '[:lower:]') --arg arch $(uname -m) -r '[.[0].files[] | select(.os == $os and .arch == $arch)| .version] | unique | .[]')
+        GO_VERSION=$(curl -s "https://go.dev/dl/?mode=json" | jq --arg os $(uname -s | tr '[:upper:]' '[:lower:]') --arg arch $(dpkg --print-architecture) -r '[.[0].files[] | select(.os == $os and .arch == $arch)| .version] | unique | .[]')
         if [ -d /usr/local/go ] && [ "$(/usr/local/go/bin/go version | cut -f3 -d' ')" != "${GO_VERSION}" ]; then
             echo Removing $(/usr/local/go/bin/go version) ...
             sudo rm -Rf /usr/local/go
@@ -115,17 +112,22 @@ Linux )
 
         if ! [ -d /usr/local/go ]; then
             echo Install Go ${GO_VERSION}...
-            curl -sL https://golang.org/dl/go${GO_VERSION}.linux-${ARCH}.tar.gz | sudo tar -C /usr/local -xz
+            curl -sL https://golang.org/dl/${GO_VERSION}.linux-$(dpkg --print-architecture).tar.gz | sudo tar -C /usr/local -xz
             /usr/local/go/bin/go version
         fi
 
 		# Install Rust
-		curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+        if [ -x "$(command -v rustup)" ]; then
+            echo "Updating rust"
+            rustup update
+        else
+		    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+        fi
 
         # WSL tools
-        if [[ $(uname -r) == *@(microsoft|wsl)* ]]; then
-            sudo apt-get install -y keychain libnss3-tools nautilus socat update-motd
-        fi
+        # if [[ "$(uname -r)" == *@(microsoft|wsl)* ]]; then
+        #     sudo apt-get install -y keychain libnss3-tools nautilus socat update-motd
+        # fi
         ;;
     esac
     ;;
