2c7136f323151abb14a8db5a92dd0fb42c93182c
Andrew Carter <acarter@microsoft.com>
2022-03-10 11:31:27 -0800

    Install Docker without prompt

--- developer.sh
+++ developer.sh
@@ -58,7 +58,7 @@ Linux )
 			curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 			echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 			sudo apt-get update
-			sudo apt-get install docker-ce docker-ce-cli containerd.io
+			sudo apt-get install -y docker-ce docker-ce-cli containerd.io
 
 			# 1Password
 			if ! [ -f /usr/share/keyrings/1password-archive-keyring.gpg ]; then
