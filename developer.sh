b070bc4065684c2c91e41904ee932c1c6afc5b98
Andrew Carter <ascarter@users.noreply.github.com>
2022-03-29 11:27:06 -0700

    Update 1Password deb repo setup

--- developer.sh
+++ developer.sh
@@ -69,18 +69,20 @@ Linux )
         if ! [ -f /usr/share/keyrings/1password-archive-keyring.gpg ]; then
           curl -sS https://downloads.1password.com/linux/keys/1password.asc | sudo gpg --dearmor --output /usr/share/keyrings/1password-archive-keyring.gpg
         fi
-        echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/1password-archive-keyring.gpg] https://downloads.1password.com/linux/debian/amd64 stable main' | sudo tee /etc/apt/sources.list.d/1password.list
+        echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/1password-archive-keyring.gpg] https://downloads.1password.com/linux/debian/$(dpkg --print-architecture) stable main" | sudo tee /etc/apt/sources.list.d/1password.list
+
         if ! [ -f /etc/debsig/policies/AC2D62742012EA22/1password.pol ]; then
           sudo mkdir -p /etc/debsig/policies/AC2D62742012EA22/
           curl -sS https://downloads.1password.com/linux/debian/debsig/1password.pol | sudo tee /etc/debsig/policies/AC2D62742012EA22/1password.pol
         fi
+
         if ! [ -f /usr/share/debsig/keyrings/AC2D62742012EA22/debsig.gpg ]; then
           sudo mkdir -p /usr/share/debsig/keyrings/AC2D62742012EA22
           curl -sS https://downloads.1password.com/linux/keys/1password.asc | sudo gpg --dearmor --output /usr/share/debsig/keyrings/AC2D62742012EA22/debsig.gpg
         fi
         sudo apt-get update
       fi
-      sudo apt-get install -y 1password
+      sudo apt-get install -y 1password 1password-cli
     fi
 
     # Add Pop packages
