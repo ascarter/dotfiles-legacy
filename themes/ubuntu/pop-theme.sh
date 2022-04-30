ef6ae02401f440937c9251d0144f8a53719687c5
Andrew Carter <ascarter@users.noreply.github.com>
2022-04-30 10:41:23 -0700

    Pop OS theme for Ubuntu 20.04

--- themes/ubuntu/pop-theme.sh
+++ themes/ubuntu/pop-theme.sh
@@ -1,31 +1,31 @@
 #!/bin/sh
 
-# Install pop theme on Ubuntu
+case "$(uname)" in
+Linux )
+  case $(lsb_release -i -s) in
+  Ubuntu )
+    # Install pop theme on Ubuntu 20.04 via PPA
+    if [ "$(lsb_release -cs)" = "focal" ]; then
+      sudo add-apt-repository ppa:system76/pop
+      sudo apt-get update
+      sudo apt-get install -y
+                    pop-launcher \
+                    pop-shell \
+                    pop-shell-shortcuts \
+                    pop-theme
 
-sudo apt-get update
-sudo apt-get install -y \
-              pop-gnome-shell-theme \
-              pop-gtk-theme \
-              pop-icon-theme \
-              pop-sound-theme
-
-# Create wallpaper
-pop_os_wallpaper=~/Pictures/Wallpapers/pop-os/wallpapers
-if ! [ -d ${pop_os_wallpaper} ]; then
-  mkdir -p $(dirname ${pop_os_wallpaper})
-  git clone https://github.com/pop-os/wallpapers.git ${pop_os_wallpaper}
-fi
-make -C ~/Pictures/Wallpapers/pop-os/wallpapers -j
-sudo make -C ~/Pictures/Wallpapers/pop-os/wallpapers install
-
-# TODO: Set gnome configuration
-echo "Recommendations for Gnome configuration:"
-echo ""
-echo "Icons: Pop Icon Theme"
-echo "Theme: Pop GTK Theme"
-echo ""
-echo "For fonts, use:"
-echo "Window Titles: Fira Sans SemiBold 10"
-echo "Interface:     Fira Sans Book 10"
-echo "Documents:     Roboto Slab Regular 11"
-echo "Monospace:     Fira Mono Regular 11"
+      # TODO: Set gnome configuration
+      echo "Recommendations for Gnome configuration:"
+      echo ""
+      echo "Icons: Pop Icon Theme"
+      echo "Theme: Pop GTK Theme"
+      echo ""
+      echo "For fonts, use:"
+      echo "Window Titles: Fira Sans SemiBold 10"
+      echo "Interface:     Fira Sans Book 10"
+      echo "Documents:     Roboto Slab Regular 11"
+      echo "Monospace:     Fira Mono Regular 11"
+    fi
+    ;;
+  esac
+esac
