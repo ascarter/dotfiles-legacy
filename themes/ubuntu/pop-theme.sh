d77a6a7d9d3832fa7d084190819d38d390e03ac6
Andrew Carter <acarter@microsoft.com>
2022-05-09 17:28:03 -0700

    Disable PPA install for pop theme

--- themes/ubuntu/pop-theme.sh
+++ themes/ubuntu/pop-theme.sh
@@ -4,15 +4,23 @@ case "$(uname)" in
 Linux )
   case $(lsb_release -i -s) in
   Ubuntu )
-    # Install pop theme on Ubuntu 20.04 via PPA
     if [ "$(lsb_release -cs)" = "focal" ]; then
-      sudo add-apt-repository ppa:system76/pop
-      sudo apt-get update
-      sudo apt-get install -y
-                    pop-launcher \
-                    pop-shell \
-                    pop-shell-shortcuts \
-                    pop-theme
+
+      # Install pop theme and shell on Ubuntu 20.04 via PPA
+      # sudo add-apt-repository ppa:system76/pop
+      # sudo apt-get update
+      # sudo apt-get install -y
+      #               pop-launcher \
+      #               pop-shell \
+      #               pop-shell-shortcuts \
+      #               pop-theme
+
+      # Install pop theme
+      sudo apt-get install -y \
+                pop-gnome-shell-theme \
+                pop-gtk-theme \
+                pop-icon-theme \
+                pop-sound-theme
 
       # TODO: Set gnome configuration
       echo "Recommendations for Gnome configuration:"
