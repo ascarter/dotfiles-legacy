6eed781979499231f36560002af38a3fea157ff5
Andrew Carter <asc@alpental.io>
2022-12-21 15:09:22 -0800

    Skip desktop apps when installing in WSL

--- scripts/workstation.sh
+++ scripts/workstation.sh
@@ -33,7 +33,7 @@ Linux )
     sudo apt-get upgrade
     sudo apt-get autoremove -y
 
-    # Setup common base packages
+    # Useful CLI packages
     sudo apt-get install -y \
                 alpine \
                 alpine-doc \
@@ -41,24 +41,18 @@ Linux )
                 apt-transport-https \
                 ca-certificates \
                 curl \
-                dconf-editor \
                 default-mta \
                 dirmngr \
+                duf \
+                exa \
                 ffmpeg \
-                fonts-firacode \
-                gnome-shell-extensions-gpaste \
-                gnome-system-log \
-                gnome-tweaks \
                 gnupg \
                 gnupg-agent \
-                gpaste \
-                gparted \
                 groff \
                 imagemagick \
                 libsecret-tools \
                 mc \
                 neofetch \
-                openssh-server \
                 software-properties-common \
                 tmux \
                 ubuntu-restricted-extras \
@@ -66,69 +60,10 @@ Linux )
                 vim-gtk3 \
                 xsel
 
-    # Newer common base packages (post-focal)
-    if ! [ "$(lsb_release -cs)" = "focal" ]; then
-      sudo apt-get install -y \
-                  duf \
-                  exa
-    fi
-
     # Microsoft GPG key
     if ! [ -f /usr/share/keyrings/microsoft-archive-keyring.gpg ]; then
       curl https://packages.microsoft.com/keys/microsoft.asc | sudo gpg --dearmor -o /usr/share/keyrings/microsoft-archive-keyring.gpg
     fi
-
-    # Microsoft Edge
-    if ! check_apt_repo "https://packages.microsoft.com/repos/edge"; then
-      curl https://packages.microsoft.com/keys/microsoft.asc | sudo gpg --dearmor -o /usr/share/keyrings/microsoft-archive-keyring.gpg
-      echo "deb [arch=amd64 signed-by=/usr/share/keyrings/microsoft.gpg] https://packages.microsoft.com/repos/edge stable main" | sudo tee /etc/apt/sources.list.d/microsoft-edge.list
-      sudo apt-get update
-    fi
-    sudo apt-get install -y microsoft-edge-stable
-
-    # 1Password
-    if ! check_apt_repo "https://downloads.1password.com"; then
-      if ! [ -f /usr/share/keyrings/1password-archive-keyring.gpg ]; then
-        curl -fsSL https://downloads.1password.com/linux/keys/1password.asc | sudo gpg --dearmor --output /usr/share/keyrings/1password-archive-keyring.gpg
-      fi
-      echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/1password-archive-keyring.gpg] https://downloads.1password.com/linux/debian/$(dpkg --print-architecture) stable main" | sudo tee /etc/apt/sources.list.d/1password.list
-
-      if ! [ -f /etc/debsig/policies/AC2D62742012EA22/1password.pol ]; then
-        sudo mkdir -p /etc/debsig/policies/AC2D62742012EA22/
-        curl -fsSL https://downloads.1password.com/linux/debian/debsig/1password.pol | sudo tee /etc/debsig/policies/AC2D62742012EA22/1password.pol
-      fi
-
-      if ! [ -f /usr/share/debsig/keyrings/AC2D62742012EA22/debsig.gpg ]; then
-        sudo mkdir -p /usr/share/debsig/keyrings/AC2D62742012EA22
-        curl -fsSL https://downloads.1password.com/linux/keys/1password.asc | sudo gpg --dearmor --output /usr/share/debsig/keyrings/AC2D62742012EA22/debsig.gpg
-      fi
-      sudo apt-get update
-    fi
-    sudo apt-get install -y 1password 1password-cli
-
-    # Sublime Text
-    if ! check_apt_repo "https://download.sublimetext.com"; then
-      curl -fsSL https://download.sublimetext.com/sublimehq-pub.gpg | sudo gpg --dearmor --output /usr/share/keyrings/sublime-text-archive-keyring.gpg
-      echo "deb [signed-by=/usr/share/keyrings/sublime-text-archive-keyring.gpg] https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
-      sudo apt-get update
-    fi
-    sudo apt-get install sublime-text sublime-merge
-
-    # Signal
-    if ! check_apt_repo "https://updates.signal.org"; then
-      curl -fsSL https://updates.signal.org/desktop/apt/keys.asc | sudo gpg --dearmor --output /usr/share/keyrings/signal-desktop-archive-keyring.gpg
-      echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/signal-desktop-archive-keyring.gpg] https://updates.signal.org/desktop/apt xenial main' | sudo tee -a /etc/apt/sources.list.d/signal.list
-      sudo apt-get update
-    fi
-    sudo apt-get install -y signal-desktop
-
-    # Onedriver
-    if ! check_apt_repo "https://download.opensuse.org/repositories/home:/jstaf"; then
-      curl -fsSL https://download.opensuse.org/repositories/home:jstaf/xUbuntu_$(lsb_release -rs)/Release.key | sudo gpg --dearmor --output /usr/share/keyrings/jstaf-archive-keyring.gpg
-      echo "deb [signed-by=/usr/share/keyrings/jstaf-archive-keyring.gpg] https://download.opensuse.org/repositories/home:/jstaf/xUbuntu_$(lsb_release -rs)/ /" | sudo tee /etc/apt/sources.list.d/jstaf.list
-      sudo apt update
-    fi
-    sudo apt-get install -y onedriver
 
     # Speedtest
     # https://www.speedtest.net/apps/cli
@@ -140,6 +75,72 @@ Linux )
     fi
     sudo apt-get install -y speedtest
     ;;
+
+    # Desktop Ubuntu (not WSL)
+    if ! (grep -iq WSL2 /proc/version); then
+      # Useful full Ubuntu applications
+      sudo apt-get install -y \
+                  dconf-editor \
+                  fonts-firacode \
+                  gnome-shell-extensions-gpaste \
+                  gnome-system-log \
+                  gnome-tweaks \
+                  gpaste \
+                  gparted \
+                  openssh-server
+
+      # Microsoft Edge
+      if ! check_apt_repo "https://packages.microsoft.com/repos/edge"; then
+        curl https://packages.microsoft.com/keys/microsoft.asc | sudo gpg --dearmor -o /usr/share/keyrings/microsoft-archive-keyring.gpg
+        echo "deb [arch=amd64 signed-by=/usr/share/keyrings/microsoft.gpg] https://packages.microsoft.com/repos/edge stable main" | sudo tee /etc/apt/sources.list.d/microsoft-edge.list
+        sudo apt-get update
+      fi
+      sudo apt-get install -y microsoft-edge-stable
+
+      # 1Password
+      if ! check_apt_repo "https://downloads.1password.com"; then
+        if ! [ -f /usr/share/keyrings/1password-archive-keyring.gpg ]; then
+          curl -fsSL https://downloads.1password.com/linux/keys/1password.asc | sudo gpg --dearmor --output /usr/share/keyrings/1password-archive-keyring.gpg
+        fi
+        echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/1password-archive-keyring.gpg] https://downloads.1password.com/linux/debian/$(dpkg --print-architecture) stable main" | sudo tee /etc/apt/sources.list.d/1password.list
+
+        if ! [ -f /etc/debsig/policies/AC2D62742012EA22/1password.pol ]; then
+          sudo mkdir -p /etc/debsig/policies/AC2D62742012EA22/
+          curl -fsSL https://downloads.1password.com/linux/debian/debsig/1password.pol | sudo tee /etc/debsig/policies/AC2D62742012EA22/1password.pol
+        fi
+
+        if ! [ -f /usr/share/debsig/keyrings/AC2D62742012EA22/debsig.gpg ]; then
+          sudo mkdir -p /usr/share/debsig/keyrings/AC2D62742012EA22
+          curl -fsSL https://downloads.1password.com/linux/keys/1password.asc | sudo gpg --dearmor --output /usr/share/debsig/keyrings/AC2D62742012EA22/debsig.gpg
+        fi
+        sudo apt-get update
+      fi
+      sudo apt-get install -y 1password 1password-cli
+
+      # Sublime Text
+      if ! check_apt_repo "https://download.sublimetext.com"; then
+        curl -fsSL https://download.sublimetext.com/sublimehq-pub.gpg | sudo gpg --dearmor --output /usr/share/keyrings/sublime-text-archive-keyring.gpg
+        echo "deb [signed-by=/usr/share/keyrings/sublime-text-archive-keyring.gpg] https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+        sudo apt-get update
+      fi
+      sudo apt-get install sublime-text sublime-merge
+
+      # Signal
+      if ! check_apt_repo "https://updates.signal.org"; then
+        curl -fsSL https://updates.signal.org/desktop/apt/keys.asc | sudo gpg --dearmor --output /usr/share/keyrings/signal-desktop-archive-keyring.gpg
+        echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/signal-desktop-archive-keyring.gpg] https://updates.signal.org/desktop/apt xenial main' | sudo tee -a /etc/apt/sources.list.d/signal.list
+        sudo apt-get update
+      fi
+      sudo apt-get install -y signal-desktop
+
+      # Onedriver
+      if ! check_apt_repo "https://download.opensuse.org/repositories/home:/jstaf"; then
+        curl -fsSL https://download.opensuse.org/repositories/home:jstaf/xUbuntu_$(lsb_release -rs)/Release.key | sudo gpg --dearmor --output /usr/share/keyrings/jstaf-archive-keyring.gpg
+        echo "deb [signed-by=/usr/share/keyrings/jstaf-archive-keyring.gpg] https://download.opensuse.org/repositories/home:/jstaf/xUbuntu_$(lsb_release -rs)/ /" | sudo tee /etc/apt/sources.list.d/jstaf.list
+        sudo apt update
+      fi
+      sudo apt-get install -y onedriver
+    fi
   esac
   ;;
 esac
